60333b973f85edf763c433533f2c2371ced42f5e
Antonina Kobzar <112172129+AntoninaKobzar@users.noreply.github.com>
2023-05-26 15:10:48 +0300

    1

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,25 +1,34 @@
 import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsSlice';
+import { getContacts } from '../../redux/selectors';
 import css from './ContactItem.module.css';
 
-const ContactItem = ({ contact,onDeleteContact}) => {
-    return (
-        <li className={css.item}>
-            <span className={css.item}>{contact.name}: </span>
-            <a href={`tel:${contact.number}`} className={css.number}>{contact.number}</a>
-            <button
-                type='button'
-                className={css.button}
-                onClick={()=>onDeleteContact(contact.id)}>Delete</button>
-        </li>
-    );
+const ContactItem = () => {
+  const dispatch = useDispatch();
+  const contact = useSelector(getContacts);
+  console.log(contact);
+  const onDeleteContact = () => dispatch(deleteContact(contact.id));
+  console.log(onDeleteContact);
+  return (
+    <li className={css.item}>
+      <span className={css.item}>{contact.name}: </span>
+      <a href={`tel:${contact.number}`} className={css.number}>
+        {contact.number}
+      </a>
+      <button type="button" className={css.button} onClick={onDeleteContact}>
+        Delete
+      </button>
+    </li>
+  );
 };
 
 ContactItem.propTypes = {
-    contact:PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-            number: PropTypes.string.isRequired,
-        }),
-    onDeleteContact: PropTypes.func.isRequired,
+  contact: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  }),
+  onDeleteContact: PropTypes.func.isRequired,
 };
 export default ContactItem;
